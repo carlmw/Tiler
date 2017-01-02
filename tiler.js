f722ac6b16bf33022032cbdaf1464df30697e317
Carl Whittaker <carl359@gmail.com>
2017-01-02 19:04:15 +0000

    Add a tile counter

--- tiler.js
+++ tiler.js
@@ -5,9 +5,12 @@ const available = '0123456789abcdefghijklmn'
   return memo;
 }, {});
 
-function toImages(str) {
-  const base24 = str.toString(24).toLowerCase().split('');
+function toBase24(str) {
+  return str.toString(24).toLowerCase().split('');
+}
 
+function toImages(base24) {
+  console.log(base24);
   return base24.map((char) => {
     const src = `${available[char][0]}.png`;
     const className = `char-${available[char][1]}`;
@@ -20,11 +23,23 @@ document.querySelector('#input').addEventListener('input', (e) => {
   const ascii = (e.target.value || '').split('').map((char) => char.charCodeAt(0)).join('');
   const base10 = parseInt(ascii, 10);
 
-  let images = '';
+  let images = '', counts = { 0: 0, 1: 0, 2: 0, 3: 0 };
 
   if (!isNaN(base10)) {
-    images = toImages(base10);
+    const base24 = toBase24(base10);
+
+    counts = base24.reduce((memo, char) => {
+      memo[available[char][0]]++;
+      return memo;
+    }, counts);
+
+
+    images = toImages(base24);
   }
+
+  Object.keys(counts).forEach(tile => {
+    document.querySelector(`#count-${tile}`).innerHTML = counts[tile];
+  });
 
   document.querySelector('#canvas').innerHTML = images;
 });
