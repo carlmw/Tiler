1f72d651f62c2cbb59e2f9c3f4e0fb819771d250
Carl Whittaker <carl359@gmail.com>
2017-01-08 18:13:45 +0000

    Add the random seed thing

--- tiler.js
+++ tiler.js
@@ -1,16 +1,18 @@
-const available = '0123456789abcdefghijklmn'
-.split('')
-.reduce((memo, char, i) => {
-  memo[char] = [Math.floor(i / 6), i % 6];
-  return memo;
-}, {});
+function seed(alphabet) {
+  return alphabet.split('')
+  .reduce((memo, char, i) => {
+    memo[char] = [Math.floor(i / 6), i % 6];
+    return memo;
+  }, {});
+}
+
+let available = seed('abcdefghijklmnopqrtuvxyz');
 
 function toBase24(str) {
-  return str.toString(24).toLowerCase().split('');
+  return str.toLowerCase().replace(/s/g, 'z').replace(/w/g, 'vv').split('');
 }
 
 function toImages(base24) {
-  console.log(base24);
   return base24.map((char) => {
     const src = `${available[char][0]}.png`;
     const className = `char-${available[char][1]}`;
@@ -19,27 +21,55 @@ function toImages(base24) {
   }).join('');
 }
 
-document.querySelector('#input').addEventListener('input', (e) => {
-  const ascii = (e.target.value || '').split('').map((char) => char.charCodeAt(0)).join('');
-  const base10 = parseInt(ascii, 10);
+function render() {
+  const value = document.querySelector('#input').value;
+  const base24 = toBase24(value);
 
   let images = '', counts = { 0: 0, 1: 0, 2: 0, 3: 0 };
 
-  if (!isNaN(base10)) {
-    const base24 = toBase24(base10);
-
-    counts = base24.reduce((memo, char) => {
-      memo[available[char][0]]++;
-      return memo;
-    }, counts);
-
-
-    images = toImages(base24);
+  if (base24.length) {
+    try {
+      counts = base24.reduce((memo, char) => {
+        memo[available[char][0]]++;
+        return memo;
+      }, counts);
+      images = toImages(base24);
+    } catch (e) {
+      images = '<span style="color:red">Invalid characters entered — only a-z is supported</span>';
+    }
   }
 
+  let totalBoxes = 0;
+
   Object.keys(counts).forEach(tile => {
-    document.querySelector(`#count-${tile}`).innerHTML = counts[tile];
+    const count = counts[tile];
+    let html = '';
+    if (count) {
+      const boxes = Math.ceil(count / 44);
+      totalBoxes += boxes;
+      html = `
+        <span>Number: ${count}</span>
+        <span>Boxes: ${boxes}</span>
+        <span>Extra: ${(boxes * 44) - count}</span>
+      `;
+    }
+    document.querySelector(`#count-${tile}`).innerHTML = html;
   });
 
+  document.querySelector('#cost').innerHTML = `£${totalBoxes * 91.5}`;
+  document.querySelector('#area').innerHTML = `${totalBoxes * 0.51}m2`;
   document.querySelector('#canvas').innerHTML = images;
+}
+
+document.querySelector('#randomise').addEventListener('click', () => {
+  const domSeed = document.querySelector('#seed');
+  domSeed.value = domSeed.value.split('').sort(() => .5 - Math.random()).join('');
+  available = seed(domSeed.value);
+  render();
 });
+
+document.querySelector('#input').addEventListener('input', () => {
+  render();
+});
+
+render();
